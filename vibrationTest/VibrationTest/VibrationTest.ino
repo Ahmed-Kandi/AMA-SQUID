29b3fe76fcd9dd76957e33e944543fd1e23ab53b
zoomingspeed <hhoussein2010@gmail.com>
2026-07-13 11:04:34 -0700

    Too Loud Test

--- vibrationTest/VibrationTest/VibrationTest.ino
+++ vibrationTest/VibrationTest/VibrationTest.ino
@@ -5,17 +5,14 @@ int tooQuietPin = 7;
 void setup() {
   // put your setup code here, to run once:
   ledcAttach(vPin, 20000, 8);
-  pinMode(tooLoudPin, INPUT_PULLUP);
-  pinMode(tooQuietPin, INPUT_PULLUP);
-  Serial.begin(9600);
+  pinMode(tooLoudPin, INPUT);
+  pinMode(tooQuietPin, INPUT);
+  digitalWrite(tooLoudPin, HIGH);
+  digitalWrite(tooQuietPin, HIGH);
 }
 
 void loop() {
-  Serial.print(digitalRead(tooLoudPin));
-  Serial.print(" ");
-  Serial.println(digitalRead(tooQuietPin));
-  delay(200);
-
+  // put your main code here, to run repeatedly:
   if (digitalRead(tooLoudPin) == 0) {
     tooLoud();
   } else if (digitalRead(tooQuietPin) == 0) {
@@ -24,11 +21,14 @@ void loop() {
 }
 
 void tooQuiet() {
-  ledcWrite(vPin, 255);
-  delay(1000);
-  ledcWrite(vPin, 0);
+  
 }
 
 void tooLoud() {
-
+  for (int i = 0; i < 255; i++) {
+    ledcWrite(vPin, i);
+    delay(10);
+    ledcWrite(vPin, 0);
+    delay(10);
+  }
 }
