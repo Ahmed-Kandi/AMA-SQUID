25092d328c9e91104fde8f537e14bded72cfbb00
Ahmed Kandil <ahmedkandil753@gmail.com>
2026-07-13 10:43:09 -0700

    Added buttons for testing

--- vibrationTest/VibrationTest/VibrationTest.ino
+++ vibrationTest/VibrationTest/VibrationTest.ino
@@ -1,9 +1,29 @@
+int vPin = 5;
+int tooLoudPin = 6;
+int tooQuietPin = 7;
+
 void setup() {
   // put your setup code here, to run once:
-
+  ledcAttach(vPin, 20000, 8);
+  pinMode(tooLoudPin, INPUT);
+  pinMode(tooQuietPin, INPUT);
+  digitalWrite(tooLoudPin, HIGH);
+  digitalWrite(tooQuietPin, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (digitalRead(tooLoudPin) == 0) {
+    tooLoud();
+  } else if (digitalRead(tooQ) == 0) {
+    tooQuiet();
+  }
+}
+
+void tooQuiet() {
+
+}
+
+void tooLoud() {
 
 }
